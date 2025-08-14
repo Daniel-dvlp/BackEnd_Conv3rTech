f3045ce74c3c7702623f9d042534d4d80e76da61
LuissyS77 <luissyhaimar1@gmail.com>
2025-08-14 08:08:41 -0500

    Servidor Funcional

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('store', 'root', '', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+    
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, // <-- aquí debe ser DB_PASS
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+  }
+);
 
-module.exports = sequelize; 
+module.exports = sequelize;
