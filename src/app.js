0f92171373733b9af5a75503ed7622848f31600f
LuissyS77 <luissyhaimar1@gmail.com>
2025-08-16 17:51:41 -0500

    tabla de direcciones de clientes con crud listo

--- src/app.js
+++ src/app.js
@@ -5,5 +5,7 @@ app.use(express.json());
 
 const UsersRoutes = require('./routes/Users/UsersRoutes');
 app.use('/api/users', UsersRoutes);
+const AddressClientsRoutes = require('./routes/Clients/AddressClientsRoutes');
+app.use('/api/address-clients', AddressClientsRoutes);
 
 module.exports = app;
