f322e80e2abdeabc3218de19e70800ef9888b2f4
sarajoharelsy0906@gmail.com <sarajoharelsy0906@gmail.com>
2025-08-20 06:38:11 -0500

    organización de rutas

--- src/app.js
+++ src/app.js
@@ -3,20 +3,19 @@ const app = express();
 
 app.use(express.json());
 
+//Miguel
 const SupplierRoutes = require('./routes/supplier/SupplierRoutes');
 app.use('/api/suppliers', SupplierRoutes);
 const PurchaseRoutes = require('./routes/purchase/PurchaseRoutes');
 app.use('/api/purchases', PurchaseRoutes);
 
+//Sarai
 const categoryRoutes = require('./routes/products_category/ProductsCategoryRoutes');
 app.use('/api/productsCategory', categoryRoutes);
 
-const categoryRoutes = require('./routes/products_category/ProductsCategoryRoutes');
-app.use('/api/productsCategory', categoryRoutes);
-
+//Luissy
 const UsersRoutes = require('./routes/Users/UsersRoutes');
 app.use('/api/users', UsersRoutes);
-
 const AddressClientsRoutes = require('./routes/Clients/AddressClientsRoutes');
 app.use('/api/address-clients', AddressClientsRoutes);
 
