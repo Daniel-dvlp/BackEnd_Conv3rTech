196e9b69a870a3b80641b93c530b7047b64b6d31
LuissyS77 <luissyhaimar1@gmail.com>
2025-08-25 19:40:46 -0500

    implementando buscador

--- src/repositories/users/UsersRepositories.js
+++ src/repositories/users/UsersRepositories.js
@@ -1,4 +1,5 @@
 const User= require('../../models/users/Users');
+const { Op } = require('sequelize');
 
 const createUser= async (user) => {
     return User.create(user);
@@ -20,10 +21,39 @@ const deleteUser = async (id) => {
     return User.destroy({ where: {  id_usuario: id } });
 }
 
+const searchUsers = async (term) => {
+    const searchTerm = (term || '').trim();
+    if (searchTerm.length === 0) {
+        return User.findAll();
+    }
+
+    const likeOperator = User.sequelize.getDialect && User.sequelize.getDialect() === 'postgres' ? Op.iLike : Op.like;
+
+    const orConditions = [
+        { documento: { [likeOperator]: `%${searchTerm}%` } },
+        { tipo_documento: { [likeOperator]: `%${searchTerm}%` } },
+        { nombre: { [likeOperator]: `%${searchTerm}%` } },
+        { apellido: { [likeOperator]: `%${searchTerm}%` } },
+        { celular: { [likeOperator]: `%${searchTerm}%` } },
+        { correo: { [likeOperator]: `%${searchTerm}%` } },
+        { id_rol: { [likeOperator]: `%${searchTerm}%` } },
+        { id_estado_usuario: { [likeOperator]: `%${searchTerm}%` } },
+    ];
+
+    const parsedId = Number(searchTerm);
+    if (!Number.isNaN(parsedId)) {
+        orConditions.push({ id_usuario: parsedId });
+    }
+
+    return User.findAll({ where: { [Op.or]: orConditions } });
+}
+
+
 module.exports = {
     createUser,
     getAllUsers,
     getUserById,
     updateUser,
-    deleteUser
+    deleteUser,
+    searchUsers
 };
