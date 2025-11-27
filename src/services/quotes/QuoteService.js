4c6576bea5604d6d87014bd778257d758fe65920
daniel-dvlp <daniel_clw@hotmail.com>
2025-11-26 20:54:15 -0500

    Correciones de proyectos roles etc

--- src/services/quotes/QuoteService.js
+++ src/services/quotes/QuoteService.js
@@ -5,6 +5,7 @@ const Product = require('../../models/products/Product');
 const ProductRepository = require('../../repositories/products/ProductRepository');
 const ServiceRepository = require('../../repositories/services/ServiceRepository');
 const ProjectService = require('../../services/projects/ProjectService');
+const MailService = require('../../services/common/MailService');
 const Project = require('../../models/projects/Project');
 const Users = require('../../models/users/Users');
 
@@ -236,7 +237,15 @@ const changeQuoteState = async (id, state, motivoAnulacion = null) => {
                     sedes: []
                 };
 
-                await ProjectService.createProject(projectData);
+                const createdProject = await ProjectService.createProject(projectData);
+                try {
+                    const recipients = await Users.findAll({ where: { id_rol: [1, 3] }, attributes: ['correo', 'nombre'] });
+                    for (const r of recipients) {
+                        const subject = `Proyecto pendiente de asignación: ${createdProject.nombre}`;
+                        const text = `Se creó el proyecto '${createdProject.nombre}' desde una cotización aprobada. Asigne responsable y equipo de trabajo.`;
+                        await MailService.sendGenericEmail({ to: r.correo, subject, text });
+                    }
+                } catch (e) {}
             }
 
             // Restar stock de productos cuando la cotización se aprueba
